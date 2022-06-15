9f8bef64f2adb81d888ad10492054985df8cac2d
Ваше имя <@exemple.com>
2022-06-16 02:43:14 +0300

    Homework_done

---
+++ Seminar_6/Task_44/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+//Если N = 5 -> 0 1 1 2 3
+//Если N = 3 -> 0 1 1
+//Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.Clear();
+Console.Write($"Введите первое N количество чисел Фибоначчи: ");
+int Ncount = Convert.ToInt32(Console.ReadLine());
+int FirstNumder = 0;
+int SecondNumber = 1;
+int result;
+Console.Write($"Если N = {Ncount} -> {FirstNumder} {SecondNumber} ");
+for (int i = 0; i < Ncount - 2; i++)
+{
+    result = FirstNumder + SecondNumber;
+    FirstNumder = SecondNumber;
+    SecondNumber = result;
+    Console.Write(result + " ");
+}
+
+Console.WriteLine();
